07778af8f90751f637c2e714a8ef7300c1884352
KhasanovTR <khasanovtimur2001@gmail.com>
2023-12-20 01:34:53 +0400

    работа с массивами Часть 2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,15 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        int[] weights = {12, 23, 43, 34, 232, 4};
+        int[] weightsCopy = {12, 23, 43, 34, 232, 4};
+        if (weights == weightsCopy) {
+            System.out.println("Массивы одинаковы");
+        } else {
+            System.out.println("Массивы не одинаковы");
+        }
+        for (int i = 0; i < weightsCopy.length; i++) {
+            System.out.println(weightsCopy[i]);
         }
     }
 }
